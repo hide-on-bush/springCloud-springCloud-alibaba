5d0157442b821047443f533c97c99ae94a3b6e47
夏世雄 <xiashixiong@fy-data.com>
2020-10-31 16:35:31 +0800

    公共模块子module cloud-api-commons构建

--- cloud-api-commons/src/main/java/com/atguigu/springCloud/entities/CommonResult.java
+++ cloud-api-commons/src/main/java/com/atguigu/springCloud/entities/CommonResult.java
@@ -1,11 +1,10 @@
-/*
+package com.atguigu.springCloud.entities;/*
  * Copyright(C) 2019 FUYUN DATA SERVICES CO.;LTD. - All Rights Reserved
  * Unauthorized copying of this file; via any medium is strictly prohibited
  * Proprietary and confidential
  * 该源代码版权归属福韵数据服务有限公司所有
  * 未经授权，任何人不得复制、泄露、转载、使用，否则将视为侵权
  */
-package com.atguigu.springCloud.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,7 +12,7 @@ import lombok.NoArgsConstructor;
 
 /**
  * @Author:夏世雄
- * @Date: 2020/10/30 16:27
+ * @Date: 2020/10/30 9:54 下午
  * @Version: 1.0
  * @E-mail: xiashixiong@fy-data.com
  * @Discription:
