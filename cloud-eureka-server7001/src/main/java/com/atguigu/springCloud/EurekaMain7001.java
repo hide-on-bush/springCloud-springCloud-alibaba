0637f145a17dfde172bd9603d6c0ba4da138933b
夏世雄 <xiashixiong@fy-data.com>
2020-11-02 08:55:21 +0800

    eureka模块构建

---
+++ cloud-eureka-server7001/src/main/java/com/atguigu/springCloud/EurekaMain7001.java
@@ -0,0 +1,26 @@
+package com.atguigu.springCloud;/*
+ * Copyright(C) 2019 FUYUN DATA SERVICES CO.;LTD. - All Rights Reserved
+ * Unauthorized copying of this file; via any medium is strictly prohibited
+ * Proprietary and confidential
+ * 该源代码版权归属福韵数据服务有限公司所有
+ * 未经授权，任何人不得复制、泄露、转载、使用，否则将视为侵权
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author:夏世雄
+ * @Date: 2020/10/31 5:03 下午
+ * @Version: 1.0
+ * @E-mail: xiashixiong@fy-data.com
+ * @Discription:
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
