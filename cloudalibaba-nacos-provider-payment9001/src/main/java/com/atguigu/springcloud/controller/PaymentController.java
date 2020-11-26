0545b784c4cfeebbe8bfb99b417fd42af0f700ce
hide-on-bush(夏世雄) <675607263@qq.com>
2020-11-26 14:36:55 +0800

    nacos作为服务注册中心，且整合openFeign进行服务调用

---
+++ cloudalibaba-nacos-provider-payment9001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright(C) 2019 FUYUN DATA SERVICES CO.;LTD. - All Rights Reserved
+ * Unauthorized copying of this file; via any medium is strictly prohibited
+ * Proprietary and confidential
+ * 该源代码版权归属福韵数据服务有限公司所有
+ * 未经授权，任何人不得复制、泄露、转载、使用，否则将视为侵权
+ */
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author:夏世雄
+ * @Date: 2020/11/26 10:49
+ * @Version: 1.0
+ * @E-mail: xiashixiong@fy-data.com
+ * @Discription:
+ **/
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/nacos/payment/{id}")
+    public String getPayment(@PathVariable("id") Integer id){
+        return "nacos registry,serverPort :" +serverPort + "\t id :" + id;
+    }
+}
